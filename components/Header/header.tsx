2f74fa6ff38ce5fe141b5f1014aab2ae191795f8
DoubleK <qk.khai2712@gmail.com>
2023-10-04 23:17:38 +0700

    fix ui for header

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -9,18 +9,18 @@ import classes from './header.module.css'
 export default function Header() {
   const appName = 'Material Mastery'
   return (
-    <Container fluid py='1rem' px='6rem' bg='white'>
+    <Container fluid py='1rem' px='6rem' bg='white' pos='fixed' top='0' left='0' right='0' className={classes.zi1000}>
       <Flex justify='space-between' align='center'>
-        <Group>
+        <Flex align='center' gap='1rem'>
           <Image component={NextImage} src={logo} alt='Logo' w='2.5rem' h='2.5rem' fit='fill'></Image>
-          <Text size='lg' fw='700' c='turquoise.6'>{appName}</Text>
-        </Group>
-        <Search content=''/>
-        <Group>
+          <Text size='1rem' fw='900' c='turquoise.6' lh='1.1875rem'>{appName}</Text>
+        </Flex>
+        <Search content='' />
+        <Flex gap='1rem' align='center'>
           <LanguagePicker />
-          <IconShoppingCart className={classes.hoverIcon}/>
-          <IconUserCircle className={classes.hoverIcon}/>
-        </Group>
+          <IconShoppingCart className={classes.hoverIcon} />
+          <IconUserCircle className={classes.hoverIcon} />
+        </Flex>
       </Flex>
     </Container>
   )
