4f36688abdab947925170088e567d6411d1809d7
DuyDangCode <21520774@gm.uit.edu.vn>
2023-11-13 08:45:00 +0700

    fix product card ui

--- components/Product/pcard.tsx
+++ components/Product/pcard.tsx
@@ -23,21 +23,41 @@ export const PCard = () => {
   return (
     <Card w={200} h={245} className={`${Styles.containerCard}`}>
       <Card.Section component={Link} href={`/products/${name}`}>
-        <Image alt="product" src={url} component={NImage} height={145}></Image>
+        <Image
+          alt="product"
+          src={url}
+          component={NImage}
+          className="h-[147px]"
+        ></Image>
       </Card.Section>
-      <Flex justify={"space-around"} direction={"column"} h={100} w={"100%"}>
-        <Text style={{ marginTop: 5, fontSize: "1rem" }}>Nothing to show</Text>
-        <Group justify="space-between">
-          <Group gap={0} align="start" justify="flex-start">
-            <Text size="20px">1000.00</Text>
-            <Text size="12px">đ</Text>
+      <Flex
+        justify={"space-around"}
+        direction={"column"}
+        h={100}
+        w={"100%"}
+        className="h-[100px] justify-around w-[100%] flex-col"
+      >
+        <Text className=" mt-2 font-mono">Nothing to show</Text>
+        <Group justify="space-between" className=" justify-between">
+          <Group
+            gap={0}
+            align="start"
+            justify="flex-start"
+            className=" items-start"
+          >
+            <Text size="20px" className=" text-[20px]">
+              1000.00
+            </Text>
+            <Text size="12px" className=" text-[12px]">
+              đ
+            </Text>
           </Group>
 
           <ActionIcon color="#E9F9F8" variant="filled" aria-label="Add">
             <IconShoppingCartPlus color="#02B1AB"></IconShoppingCartPlus>
           </ActionIcon>
         </Group>
-        <Group justify="space-between">
+        <Group justify="space-between" className=" justify-between">
           <Rating value={value} onChange={setValue} />
           <Group>
             <Divider size="sm" orientation="vertical" />
