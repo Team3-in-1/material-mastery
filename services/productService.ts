f93e78d8fdfc082997f7081a013053edf8c2e82f
Duong Dinh <duong.dinh@affina.com.vn>
2023-11-14 08:56:10 +0700

    call api for product page

---
+++ services/productService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { Product } from "@/utils/response";
+import { constant } from "@/utils/constant";
+
+const getAllProducts = async (): Promise<Product[]> => {
+    return await axios.get(`${constant.BASE_URL}/product`, {
+        headers: {
+            'x-api-key': constant.API_KEY
+        }
+    })
+    .then(res => res.data.metadata)
+    .catch(error => {throw new Error(error.response.data.message)})
+}
+
+export const productService = {
+    getAllProducts
+}
