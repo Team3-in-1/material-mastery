8ba36cd4998f4ca4bf0594beff66d949a638115a
Duong Dinh <duong.dinh@affina.com.vn>
2023-11-14 14:46:18 +0700

    add cart page

---
+++ app/cart/layout.tsx
@@ -0,0 +1,21 @@
+import '../global.css'
+
+import { Container } from "@mantine/core";
+
+const CartLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className={`page-container`}>
+            <Container fluid py={70} px={100}>
+                <div className='mt-4 px-2'>
+                    {children}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default CartLayout
