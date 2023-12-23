d8c2b9d7ad8e4d546cda06257f8aca786ae4e75a
DoubleK <qk.khai2712@gmail.com>
2023-12-24 01:16:48 +0700

    config next for deploy

--- next.config.js
+++ next.config.js
@@ -8,35 +8,35 @@ const nextConfig = {
     domains: ['biiibo.com', 'drive.google.com', 'd3q01gc7kwv7n6.cloudfront.net', 'iqvinc.com'],
   },
   //http://mm.khangdev.id.vn/';
-  async redirects() {
-    return [
-      {
-        source: '/staff',
-        destination: '/staff/dashboard/revenue',
-        permanent: true,
-      },
-      {
-        source: '/staff/order',
-        destination: '/staff/order/online',
-        permanent: true,
-      },
-      {
-        source: '/manager',
-        destination: '/manager/dashboard/revenue',
-        permanent: true,
-      },
-      {
-        source: '/manager/order',
-        destination: '/manager/order/online',
-        permanent: true,
-      },
-      {
-        source: '/manager/warehouse',
-        destination: '/manager/warehouse/instock',
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/staff',
+  //       destination: '/staff/dashboard/revenue',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/staff/order',
+  //       destination: '/staff/order/online',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/manager',
+  //       destination: '/manager/dashboard/revenue',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/manager/order',
+  //       destination: '/manager/order/online',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/manager/warehouse',
+  //       destination: '/manager/warehouse/instock',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
