29e4bc13c1a51bda38a914b5e076980d3b65ea38
Thành Duy <21520774@gm.uit.edu.vn>
2023-12-24 15:40:54 +0700

    Update nav.tsx

--- components/Account/nav.tsx
+++ components/Account/nav.tsx
@@ -1,15 +1,17 @@
 'use client';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { IconUser, IconChecklist } from '@tabler/icons-react';
 import { Box, NavLink, Stack } from '@mantine/core';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import UserContext from '@/contexts/UserContext';
 
 // interface HandleRouterInterface {
 //   [id: number]: Function;
 // }
 
 const AccountNav = ({ namePage }: { namePage: string }) => {
+  const { user } = useContext(UserContext);
   const [active, setActive] = useState(namePage);
 
   return (
@@ -35,17 +37,20 @@ const AccountNav = ({ namePage }: { namePage: string }) => {
         style={active === 'details' ? { color: '#02B1AB' } : { color: '#000' }}
         component={Link}
       />
-      <NavLink
-        active={active === 'orders'}
-        label='Đơn hàng'
-        leftSection={<IconChecklist size='1rem' stroke={1.5} />}
-        className='rounded-[12px] text-black'
-        h={50}
-        mb={12}
-        style={active === 'orders' ? { color: '#02B1AB' } : { color: '#000' }}
-        href={'/account/orders'}
-        component={Link}
-      />
+
+      {user?.roles[0] === 'customer' && (
+        <NavLink
+          active={active === 'orders'}
+          label='Đơn hàng'
+          leftSection={<IconChecklist size='1rem' stroke={1.5} />}
+          className='rounded-[12px] text-black'
+          h={50}
+          mb={12}
+          style={active === 'orders' ? { color: '#02B1AB' } : { color: '#000' }}
+          href={'/account/orders'}
+          component={Link}
+        />
+      )}
     </Stack>
   );
 };
