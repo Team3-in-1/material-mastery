97c8ab1038c0417486d027e8e47865446f2b0f5c
Thành Duy <21520774@gm.uit.edu.vn>
2023-12-24 15:41:10 +0700

    Update loggedHeader.tsx

--- components/Header/loggedHeader.tsx
+++ components/Header/loggedHeader.tsx
@@ -72,7 +72,7 @@ const LoggedHeader = ({ user, setUser }: { user: any; setUser: any }) => {
   return (
     <Flex gap='1rem' align='center' className='hidden-mobile'>
       {/* <LanguagePicker /> */}
-      {user?.roles[0] != 'manager' && (
+      {user?.roles[0] != 'manager' && user?.roles[0] != 'staff' && (
         <div className=' relative w-[25px] h-[25px] cursor-pointer'>
           <IconShoppingCart
             onClick={() => {
@@ -101,25 +101,23 @@ const LoggedHeader = ({ user, setUser }: { user: any; setUser: any }) => {
           <IconUserCircle color='#02B1AB' className={classes.hoverIcon} />
         </Menu.Target>
         <Menu.Dropdown>
-          {user?.roles[0] != 'manager' && (
-            <>
-              <Menu.Item
-                leftSection={
-                  <IconUser style={{ width: rem(14), height: rem(14) }} />
-                }
-                onClick={() => handleOnClickOnMenu('details')}
-              >
-                Thông tin tài khoản
-              </Menu.Item>
-              <Menu.Item
-                leftSection={
-                  <IconChecklist style={{ width: rem(14), height: rem(14) }} />
-                }
-                onClick={() => handleOnClickOnMenu('orders')}
-              >
-                Đơn hàng
-              </Menu.Item>
-            </>
+          <Menu.Item
+            leftSection={
+              <IconUser style={{ width: rem(14), height: rem(14) }} />
+            }
+            onClick={() => handleOnClickOnMenu('details')}
+          >
+            Thông tin tài khoản
+          </Menu.Item>
+          {user?.roles[0] != 'manager' && user?.roles[0] != 'staff' && (
+            <Menu.Item
+              leftSection={
+                <IconChecklist style={{ width: rem(14), height: rem(14) }} />
+              }
+              onClick={() => handleOnClickOnMenu('orders')}
+            >
+              Đơn hàng
+            </Menu.Item>
           )}
 
           <Menu.Item
